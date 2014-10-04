8cd9d7d44f02cff7da6d71d79c5537cc27ee4594
Matt Carroll <mattc@yelp.com>
2014-10-04 02:08:11 +0100

    Added oh-my-zsh dependency

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 function exists-or-clone {
-  local shortname=$(echo $1 | sed -E 's|.+/(.+)|\1|' | sed -E 's/\.git//')
-  if [ ! -d "plugins/$shortname" ]; then
-    echo "$shortname does not exist, cloning"
-    git clone $1 plugins/$shortname
+  if [ -z $2 ]; then
+    local dest="plugins/$(echo $1 | sed -E 's|.+/(.+)|\1|' | sed -E 's/\.git//')"
+  else
+    local dest=$2
+  fi
+
+  if [ ! -d "$dest" ]; then
+    echo "$dest does not exist, cloning"
+    git clone $1 $dest
   fi
 }
 
 read -d '' remotes <<EOF
+git://github.com/robbyrussell/oh-my-zsh.git ${HOME}/.oh-my-zsh
 https://github.com/hchbaw/opp.zsh
 EOF
 
